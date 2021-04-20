c1e0a74d73b9556f9e53a697978c49fb68882846
FilsanMadar <filsanmadar@gmail.com>
2021-04-20 19:07:26 -0400

    final

--- src/main/java/cookies/CookieOrder.java
+++ src/main/java/cookies/CookieOrder.java
@@ -1,4 +1,20 @@
 package cookies;
 
 public class CookieOrder {
+
+    private int numBoxes;
+    private Object variety;
+
+    public CookieOrder(String localVariety, int localNumBoxes) {
+        this.numBoxes = localNumBoxes;
+        this.variety = localVariety;
+    }
+
+    public int getNumBoxes() {
+        return numBoxes;
+    }
+
+    public Object getVariety() {
+        return this.variety;
+    }
 }
